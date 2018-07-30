25817211eaef66b5fa9b1cd0873df48235a85822
Casey Van-Roose <caseyvanroose@Caseys-MacBook-Pro.local>
2018-07-30 10:32:21 +0100

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,38 @@
 class Restaurant
   attr_accessor :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    all.find {|restuarants| restuarants.name == name}
+  end
+
+  def customers
+    total_customers = Review.all.select {|customers| customers.restaurant == self}
+    total_customers.map {|custom| custom.customer}
+  end
+
+  def reviews
+    stars = Review.all.select {|reviews| reviews.restaurant == self}
+    #stars.inject(0) { |sum, el| sum + el } / stars.size
+  end
+
+  def average_star_rating
+    reviews.map {|ratings| ratings.rating}
+  end
+
+  def longest_review
+    all_reviews = reviews.map {|contents| contents.content}
+    all_reviews.max_by(&:length)
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,17 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+casey = Customer.new("Casey", "vanRoose")
+mike = Customer.new("Mike", "Rumbles")
+michael = Customer.new("Mike", "Lee")
+
+gbk = Restaurant.new("GBK")
+pizza_express = Restaurant.new("Pizza Express")
+
+good = Review.new(casey, gbk, 4, "Good")
+bad = Review.new(casey, pizza_express, 2, "Bad!")
+avarage = Review.new(mike, gbk, 2, "Average!")
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
