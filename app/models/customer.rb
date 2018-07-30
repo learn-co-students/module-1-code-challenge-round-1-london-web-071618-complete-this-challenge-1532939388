c305a14d4b82956aceeac11245f4b2da732b0a33
ritz-w <ritz.wu@outlook.com>
2018-07-30 10:26:55 +0100

    completed challenge

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,59 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.find_by_name(name)
+    self.all.find{|c| c.full_name == name}
+  end
+
+  def self.find_all_by_first_name(name)
+    self.all.select{|c| c.first_name == name}.map{|c| c.full_name}
+  end
+
+  def self.all_names
+    self.all.map{|c| c.full_name}
+  end
+
+  def add_review(restaurant, content, rating)
+    Review.new(self, restaurant, rating, content)
+  end
+
+  def num_reviews
+    Review.all.select {|r| r.customer == self}.count
+  end
+
+  def restaurants
+    Review.all.select {|r| r.customer == self}.map {|r| r.restaurant}.uniq
+  end
 end
+
+# - `Customer#add_review(restaurant, content, rating)`
+#   - given a **restaurant object**, some review content (as a string), and a star rating (as an integer), creates a new review and associates it with that customer and restaurant.
+# - `Customer#num_reviews`
+#   - Returns the total number of reviews that a customer has authored
+# - `Customer#restaurants`
+#   - Returns a **unique** array of all restaurants a customer has reviewed
+
+# - `Customer.all`
+#   - should return **all** of the customer instances
+# - `Customer.find_by_name(name)`
+#   - given a string of a **full name**, returns the **first customer** whose full name matches
+# - `Customer.find_all_by_first_name(name)`
+#   - given a string of a first name, returns an **array** containing all customers with that first name
+# - `Customer.all_names`
+#   - should return an **array** of all of the customer full names
