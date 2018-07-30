4a31cf1c0fa340289ce8e35c60d5687093889966
github username <github email address>
2018-07-30 10:32:04 +0100

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,50 @@
 class Restaurant
   attr_accessor :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+      Restaurant.all.find {|restaurant| restaurant.name == name}
+  end
+
+  def customers
+      all_reviews = Review.all.select {|review| review.restaurant == self}
+      all_reviews.map {|review| review.customer}.uniq
+  end
+
+  def reviews
+      Review.all.select {|review| review.restaurant == self}
+  end
+
+  def average_star_rating
+      count = 0
+      reviews.each {|review| count += review.rating}
+      count/ (reviews.size)
+  end
+
+
+
 end
+
+
+
+#
+#
+#
+# - `Restaurant#customers`
+#   - Returns a **unique** list of all customers who have reviewed a particular restaurant.
+# - `Restaurant#reviews`
+#   - returns an array of all reviews for that restaurant
+# - `Restaurant#average_star_rating`
+#   - returns the average star rating for a restaurant based on its reviews
+# - `Restaurant#longest_review`
+#   - returns the longest review content for a given restaurant
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,27 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+
+ali = Customer.new("ali", "aziz")
+sarah = Customer.new("sarah", "jacob")
+maduri_vassaramo = Customer.new("maduri", "vassaramo")
+maduri = Customer.new("maduri", "vass")
+
+
+restaurant1 = Restaurant.new("restaurant1")
+restaurant2 = Restaurant.new("restaurant2")
+restaurant3 = Restaurant.new("restaurant3")
+restaurant4 = Restaurant.new("restaurant4")
+
+
+review1 = Review.new("review1", sarah, restaurant1, 5, "content")
+review2 = Review.new("review1", ali, restaurant1,2, "content")
+review3 = Review.new("review1", ali, restaurant3, 5, "content")
+review4 = Review.new("review1", maduri, restaurant4, 4, "content")
+
+
+
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
