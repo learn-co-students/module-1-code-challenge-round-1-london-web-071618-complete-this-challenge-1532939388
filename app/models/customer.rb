0d582cd0096d41ffc3452fbc3fdc079c83e362d3
Adham <admuaq@gmail.com>
2018-07-30 10:36:29 +0100

    Submit

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,44 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|n| (n.first_name + " " + n.last_name) == name}
+  end
+
+  def self.find_all_by_first_name(name)
+    self.all.select {|n| n.first_name == name}
+  end
+
+  def self.all_names
+    self.all.map {|n| n.first_name + " " + n.last_name}
+  end
+
+  def add_review(content, restaurant, rating)
+    review = Review.new(content, self, restaurant, rating)
+    Review.all << review
+  end
+
+  def num_reviews
+    Review.all.select {|review| review.customer == self}.count
+  end
+
+  def restaurants
+    Review.all.select {|review| review.restaurant}.uniq
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,35 @@
 class Review
-  
+
+attr_reader :customer
+attr_accessor :content, :restaurant, :rating
+
+@@all = []
+
+def initialize(content, customer, restaurant, rating)
+  @customer = customer
+  @rating = rating
+  @restaurant = restaurant
+  @content = content
 end
 
+def self.all
+  @@all
+end
+
+def review_restaurant
+
+end
+
+def review_rating
+
+end
+
+def review_content
+
+end
+
+def review_customer
+  self.all.select {|review| review.customer.first_name}
+end
+
+end
