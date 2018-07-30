b921d2bdf960eefc1a9f108abc2b86e83ad2701a
github username <github email address>
2018-07-30 10:52:26 +0100

    done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,39 @@
 class Customer
   attr_accessor :first_name, :last_name
-
+@@all=[]
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all<<self
   end
-
+#works
   def full_name
     "#{first_name} #{last_name}"
   end
+  #works
+  def self.all
+    @@all
+  end
+  def find_by_name(name)
+    Customer.all.find_by{|customer| customer.name == full.name}.first
+  end
+  def find_all_by_first_name(name)
+    customer.all.find_by {|customer| customer.first_name==name}
+  end
+  def all_names
+customers_names={}
+end
+def add_review(restaurant, content, rating)
+Review.new(self, restaurant, rating)
+end
+
+
+def num_reviews
+  Review.all.map {|review| review.customers }
+end
+
+
+def restaurant
+  Review.all.select {|review| review.custumer==review.restaurant}.uniqu
+end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,21 @@
 class Review
-  
+  @@all=[]
+attr_accessor :customer, :restaurant, :rating
+attr_reader
+
+  def initialize(customer, restaurant, rating)  
+    @customer = customer
+    @restaurant=restaurant
+    @rating = rating
+    @@all << self
+end
+#works
+def self.all
+  @@all
+end
+def restaurant
+  restaurant_review =restaurant.all.find {|restaurant| restaurant.name = self}
+  restaurant_review.map {|customer| customer.name == name}
 end
 
+end
